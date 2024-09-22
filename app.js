8749999a62861623a7e06f1c66bb28d288b9606b
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-09-21 17:15:37 -0700

    Refactor routes in app.js and add lesson1 controller

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
 
-app.get('/', lesson1Controller.helloRoute);
-
-app.get('/goodbye', lesson1Controller.goodbyeRoute);
+app.use('/', require('./routes'));
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
